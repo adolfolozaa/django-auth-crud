46ccdcce9552a7f064b79fc36a46cb9ca449973d
Adolfo <adolfolozaa@gmail.com>
2022-09-22 13:27:15 -0500

    Correccion tareas completadas 2

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -88,15 +88,13 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'default': 
     ###'ENGINE': 'django.db.backends.sqlite3',
     ###    'NAME': BASE_DIR / 'db.sqlite3', }
         dj_database_url.config( 
-            default='postgresql://postgres:postgres@localhost/postgres', conn_max_age=600)}
+            default='postgresql://postgres:postgres@localhost/postgres', conn_max_age=600)
 
-        
-
-    }
+        }
 
 
 
